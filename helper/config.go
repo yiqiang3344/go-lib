eec1f22b93d2e6b40a17e9e5a7dc76d9c3fad8f3
junqiang.yi <junqiang.yi@xinfei.cn>
2021-04-29 19:12:15 +0800

    配置中心加热更新

--- helper/config.go
+++ helper/config.go
@@ -1,62 +1,87 @@
 package helper
 
 import (
+	"fmt"
 	"github.com/micro/go-micro/v2/config"
 	"github.com/micro/go-micro/v2/config/reader"
+	"github.com/micro/go-micro/v2/config/source/etcd"
+	"github.com/micro/go-micro/v2/config/source/file"
+	"go.uber.org/zap"
+	"log"
 	"os"
 	"strings"
 )
 
+var Cfg config.Config
+
+var prefixs = []string{"cfg"}
+
 func InitCfg() {
-	_ = config.LoadFile("config/" + os.Getenv("ENV") + ".yaml")
+	err := config.Load(
+		etcd.NewSource(
+			etcd.WithAddress("127.0.0.1:2379"),
+			etcd.WithPrefix("/common/config"),
+			etcd.StripPrefix(true),
+		),
+		etcd.NewSource(
+			etcd.WithAddress("127.0.0.1:2379"),
+			etcd.WithPrefix("/xyf-robot-srv/config"),
+			etcd.StripPrefix(true),
+		),
+		file.NewSource(file.WithPath("config/"+os.Getenv("ENV")+".yaml")),
+	)
+	if err != nil {
+		log.Fatalf("log init failed:", err.Error())
+	}
+
+	go hotUpdate()
+}
+
+func hotUpdate() {
+	for {
+		//启动热更新
+		w, err := config.Watch("cfg")
+		if err != nil {
+			ErrorLog("配置热更新失败:"+err.Error(), RunFuncName())
+			return
+		}
+		v, err := w.Next()
+		if err != nil {
+			ErrorLog("配置热更新下一个失败:"+err.Error(), RunFuncName())
+			return
+		}
+		before := fmt.Sprint(config.Get(prefixs...).StringMap(map[string]string{}))
+		err = config.Sync()
+		if err != nil {
+			ErrorLog("配置热更新同步失败:"+err.Error(), RunFuncName())
+			return
+		}
+		DebugLog(
+			"配置热更新成功",
+			RunFuncName(),
+			zap.String("beforeCfgData", before),
+			zap.String("afterCfgData", fmt.Sprint(v.StringMap(map[string]string{}))),
+		)
+	}
 }
 
 func GetCfgString(route string) string {
-	arr, _len := parseRoute(route)
-	if _len == 0 {
-		return ""
-	}
-	cfg := getCfg(_len, arr)
-	return cfg.String("")
+	return getCfg(route).String("")
 }
 
 func GetCfgInt(route string) int {
-	arr, _len := parseRoute(route)
-	if _len == 0 {
-		return 0
-	}
-	cfg := getCfg(_len, arr)
-	return cfg.Int(0)
+	return getCfg(route).Int(0)
 }
 
-func parseRoute(route string) ([]string, int) {
-	arr := strings.Split(route, ".")
-	return arr, len(arr)
+func parseRoute(route string) []string {
+	arr := prefixs[0:]
+	for _, v := range strings.Split(route, ".") {
+		arr = append(arr, v)
+	}
+	return arr
 }
 
-func getCfg(len int, arr []string) reader.Value {
-	var ret reader.Value
-	switch len {
-	case 1:
-		ret = config.Get(arr[0])
-	case 2:
-		ret = config.Get(arr[0], arr[1])
-	case 3:
-		ret = config.Get(arr[0], arr[1], arr[2])
-	case 4:
-		ret = config.Get(arr[0], arr[1], arr[2], arr[3])
-	case 5:
-		ret = config.Get(arr[0], arr[1], arr[2], arr[3], arr[4])
-	case 6:
-		ret = config.Get(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5])
-	case 7:
-		ret = config.Get(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6])
-	case 8:
-		ret = config.Get(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6], arr[7])
-	case 9:
-		ret = config.Get(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6], arr[7], arr[8])
-	case 10:
-		ret = config.Get(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6], arr[7], arr[8], arr[9])
-	}
-	return ret
+func getCfg(route string) reader.Value {
+	arr := parseRoute(route)
+	return config.Get(arr...)
 }
